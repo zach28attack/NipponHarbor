ffc21a7dd1154e2cf24f2e47748d64c31163a5bf
Zachary Casares <rrumble250@gmail.com>
2023-12-06 15:09:53 -0500

    setup react router with root component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-import "./App.css";
-import ArticleIndex from "./pages/ArticleIndex";
+import Class from "./App.module.css";
+import {Outlet} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <ArticleIndex />;
+  return (
+    <div className={Class.container}>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -24,6 +24,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
