72ccceb9ea573a4cfd31675d39cd60e1af1e04ae
Zachary Casares <rrumble250@gmail.com>
2023-12-06 15:10:08 -0500

    create navbar

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import Class from "./Navbar.module.css";
+import {Link} from "react-router-dom";
+import {MdAccountBox} from "react-icons/md";
+import {IoIosArrowDown} from "react-icons/io";
+
+export default function Navbar() {
+  return (
+    <div className={Class.container}>
+      <div className={Class.icon}>
+        <span>NipponHarbor</span>
+      </div>
+      <div className={Class.navItems}>
+        <div className={Class.navItem}>
+          <Link to="/articles">Articles</Link>
+        </div>
+        <div className={Class.navItem}>
+          <Link to="/">Flashcards</Link>
+        </div>
+      </div>
+      <div className={Class.accountDropdown}>
+        <div className={Class.header}>
+          <MdAccountBox />
+          <IoIosArrowDown />
+        </div>
+        {/* <div className={Class.dropdown}>
+            <Link to="/">Login</Link>
+            <Link to="/">Register</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">Profile</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">Logout</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">Settings</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">About</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">Contact</Link>
+          </div>
+          <div className={Class.dropdown}>
+            <Link to="/">Privacy</Link>
+          </div> */}
+      </div>
+    </div>
+  );
+}
