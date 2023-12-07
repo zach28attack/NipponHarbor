edddd9b8d9d9081cd15bfb3d79040b7c8c3f15b9
Zachary Casares <rrumble250@gmail.com>
2023-12-07 16:43:20 -0500

    render articles and format

--- client/src/pages/ArticleIndex.jsx
+++ client/src/pages/ArticleIndex.jsx
@@ -1,5 +1,24 @@
+import ArticleCard from "../components/articles/ArticleCard";
 import Class from "./ArticleIndex.module.css";
 
 export default function ArticlesIndex() {
-  return;
+  const article = {
+    title: "茨城 車突入事件 容疑者の母親「一緒に出かけた帰りに突然」",
+    img: "/Yen_Img.jpg",
+    author: "Authur Morgan",
+    date: "12月8日 4時39分",
+    category: "Politics",
+  };
+  return (
+    <div className={Class.container}>
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+      <ArticleCard article={article} />
+    </div>
+  );
 }
