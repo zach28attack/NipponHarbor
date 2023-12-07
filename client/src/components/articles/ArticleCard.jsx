8515c49f28e30434af7c03e7df7e4a7709845050
Zachary Casares <rrumble250@gmail.com>
2023-12-07 16:43:06 -0500

    add articles card with styling

---
+++ client/src/components/articles/ArticleCard.jsx
@@ -0,0 +1,21 @@
+import Class from "./ArticleCard.module.css";
+import {Link} from "react-router-dom";
+
+export default function ArticleCard({article}) {
+  return (
+    <div className={Class.container}>
+      <img src={article.img} />
+      <div className={Class.content}>
+        <header>
+          <Link to="/articles">
+            <span>{article.title}</span>
+          </Link>
+        </header>
+        <footer>
+          <time>{article.date}</time>
+          <span>NEW</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
