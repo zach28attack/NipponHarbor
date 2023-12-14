22e78ed7862634eebcadcf32c2ed3c9ad45faf3b
Zachary Casares <rrumble250@gmail.com>
2023-12-13 21:55:17 -0500

    get json data from python script and pass to client

---
+++ server/controller/articles.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const {exec} = require("child_process");
+
+exports.getArticles = async (req, res) => {
+  let articles = [];
+  exec("/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 ./scrapers/article_web_scraper.py", (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fs.readFile("articles.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500);
+        return;
+      }
+      articles = JSON.parse(data);
+      res.status(200).json({
+        articles: articles,
+      });
+    });
+  });
+};
