0d23a755fef32366c6526df49adca02b66789a4d
Zachary Casares <rrumble250@gmail.com>
2023-12-13 21:54:53 -0500

    get articles and save data in json file

--- server/scrapers/article_web_scraper.py
+++ server/scrapers/article_web_scraper.py
@@ -6,28 +6,26 @@ import json
 
 url = 'https://www3.nhk.or.jp/news/'
 
-def get_news_headlines():
-    print('Scraping the web...')
-    # Get the html from the page
-    response = requests.get(url)
-    # Parse the html using beautiful soup and store in variable `soup`
-    soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
-    # Find all the headlines and store in variable `news_headlines`
-    news_headlines = soup.find_all('em', {'class':'title'})
-    # Process the headlines
-    headlines = process_headlines(news_headlines)
-    print('Returning processed headlines in json format')
-    # Return the headlines in json format
-    with open('headlines.json', 'w') as json_file:
-        json.dump(headlines, json_file)
+def get_articles():
+  print('Scraping the web...')
 
-def process_headlines(headlines):
-    print('Processing the headlines...')
-    processed_headlines = []
-    for headline in headlines:
-        processed_headlines.append(headline.text)
-    print('Headline processes.')
-    return processed_headlines
+  response = requests.get(url)
 
-get_news_headlines()
+  soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
 
+  news_html_parsed = soup.find('div', {'class':'content--items'}).find_all('li')
+
+  news_list = []
+
+  for article in news_html_parsed:
+    obj = {
+        'headline': article.find('em', {'class':'title'}).text,
+        'img_url': article.find('img')['data-src'],
+        'url': article.find('a').get('href'),
+    }
+    print(obj)
+    news_list.append(obj)
+  with open('articles.json', 'w') as json_file:
+    json.dump(news_list, json_file)
+
+get_articles()
