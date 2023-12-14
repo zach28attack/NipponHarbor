7b8125d3f91ec36236d83e2169c804fd80fc4e1d
Zachary Casares <rrumble250@gmail.com>
2023-12-14 14:12:07 -0500

    comment out script execution code

--- server/controller/articles.js
+++ server/controller/articles.js
@@ -3,21 +3,23 @@ const {exec} = require("child_process");
 
 exports.getArticles = async (req, res) => {
   let articles = [];
-  exec("/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 ./scrapers/article_web_scraper.py", (err) => {
+  // TODO: move script execution out of controller and into a scheduler
+  //
+  // exec("/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 ./scrapers/article_web_scraper.py", (err) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  fs.readFile("articles.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
+      res.status(500);
       return;
     }
-    fs.readFile("articles.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500);
-        return;
-      }
-      articles = JSON.parse(data);
-      res.status(200).json({
-        articles: articles,
-      });
+    articles = JSON.parse(data);
+    res.status(200).json({
+      articles: articles,
     });
   });
+  // });
 };
